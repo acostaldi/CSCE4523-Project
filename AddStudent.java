5fcdcdd1bbdcc4fa0e8257cab5291cd25544894a
LTShrub <amadeom.costaldi@gmail.com>
2023-04-26 13:05:02 -0500

    Updated

--- AddStudent.java
+++ AddStudent.java
@@ -14,8 +14,8 @@ public class AddStudent {
 
       // For debugging purposes: Show the database before the insert
       StringBuilder builder = new StringBuilder();
-      String query1 = "SELECT * from ITEM";
-      builder.append("<br> Table ITEM before:" + myDB.query(query1) + "<br>");
+      String query1 = "SELECT * from Student";
+      builder.append("<br> Table Student before:" + myDB.query(query1) + "<br>");
 
       // Parse input string to get restauranrestaurant Name and Address
       String name;
@@ -30,10 +30,10 @@ public class AddStudent {
 
       // Insert the new restaurant
       String input = "'" + name + "','" + student_id + "','" + major + "'";
-      myDB.insert("ITEM", input); // insert new student
+      myDB.insert("Student", input); // insert new student
 
       // For debugging purposes: Show the database after the insert
-      builder.append("<br><br><br> Table ITEM after:" + myDB.query(query1));
+      builder.append("<br><br><br> Table Student after:" + myDB.query(query1));
       System.out.println(builder.toString());
 
       myDB.disConnect();
