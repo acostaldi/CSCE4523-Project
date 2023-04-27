3fad5ee4fc7e74484190e8ac2d24f9e6aeb64f5e
LTShrub <amadeom.costaldi@gmail.com>
2023-04-26 19:15:18 -0500

    Added Add Jobs

--- AddJob.java
+++ AddJob.java
@@ -1,6 +1,6 @@
 import java.sql.*;
 
-public class AddStudent {
+public class AddJob {
    // The main program that inserts a restaurant
    public static void main(String[] args) throws SQLException {
       String Username = "amcostal"; // Change to your own username
@@ -13,28 +13,36 @@ public class AddStudent {
 
       // For debugging purposes: Show the database before the insert
       StringBuilder builder = new StringBuilder();
-      String query1 = "SELECT * from Students;";
+      String query1 = "SELECT * from Jobs;";
       //System.out.println("DEBUG(AddStudent L11):" + query1);
-      builder.append("<br> Table Student before:" + myDB.query(query1) + "<br>");
+      builder.append("<br> Table Jobs before:" + myDB.query(query1) + "<br>");
 
       // Parse input string to get restauranrestaurant Name and Address
-      String name;
-      String student_id;
-      String major;
+      String CompanyName;
+      String JobTitle;
+      String Salary;
+      String DesiredMajor;
 
       // Read command line arguments
       // args[0] is the first parameter
-      name = args[0];
-      student_id = args[1];
-      major = args[2];
+      CompanyName = args[0];
+      JobTitle = args[1];
+      Salary = args[2];
+      DesiredMajor = args[3];
+
+      String q = "select IFNULL(max(JobId), 0) as max_id from Jobs";
+      ResultSet result = myDB.rawQuery(q);
+      int job_id = 1;
+      if (result.next()) // get first row of result set
+         job_id += result.getInt("max_id");
 
       // Insert the new restaurant
-      String input = "'" + student_id + "','" + name + "','" + major + "'";
-      //System.out.println("DEBUG AddStudent L34: " + input);
-      myDB.insert("Students", input); // insert new student
+      String input = "'" + job_id + "','" + CompanyName + "','" + JobTitle + "','" + Salary  + "','" + DesiredMajor + "'";
+      //System.out.println("DEBUG AddJob L34: " + input);
+      myDB.insert("Jobs", input); // insert new student
 
       // For debugging purposes: Show the database after the insert
-      builder.append("<br><br><br> Table Students after:" + myDB.query(query1));
+      builder.append("<br><br><br> Table Jobs after:" + myDB.query(query1));
       System.out.println(builder.toString());
 
       myDB.disConnect();
---
+++ AddJob.php
@@ -0,0 +1,32 @@
+<html>
+    <body>
+        <h1>Enter Student Information.</h1>
+        
+        <form action="AddJob.php" method="post">
+            Company Name: <input type="text" name="CompanyName"><br>
+            Job Title: <input type="text" name="JobTitle"><br>
+            Salary: <input type="text" name="Salary"><br>
+            Desired Major: <input type="text" name="major"><br>
+            <input name="submit" type="submit" >
+        </form>
+    <body>
+<html>
+
+<?php
+if (isset($_POST['submit'])) 
+{
+    // replace ' ' with '\ ' in the strings so they are treated as single command line args
+    $CompanyName = escapeshellarg($_POST[CompanyName]);
+    $JobTitle = escapeshellarg($_POST[JobTitle]);
+    $Salary = escapeshellarg($_POST[Salary]);
+    $major = escapeshellarg($_POST[major]);
+
+    $command = 'java -cp .:mysql-connector-java-5.1.40-bin.jar AddJob ' . $CompanyName . ' ' . $JobTitle . ' ' . $Salary . ' ' . $major;
+
+    // remove dangerous characters from command to protect web server
+    $escaped_command = escapeshellcmd($command);
+    echo "<p>command: $command <p>"; 
+    // run jdbc_insert_item.exe
+    system($escaped_command);           
+}
+?>
