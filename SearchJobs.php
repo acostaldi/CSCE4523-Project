b3db662b2ab7ad58edad4acdae241ddce182dda1
bentonballer <131487483+bentonballer@users.noreply.github.com>
2023-04-26 22:47:54 -0500

    Search Jobs

---
+++ SearchJobs.php
@@ -0,0 +1,39 @@
+<html>
+    <style>
+        body {
+            background-color: beige;
+        }
+        </style>
+    <body>
+        <h1>Enter Job Major.</h1>
+        
+        <form action="SearchJobs.php" method="post">
+            Major: 
+            <input list="majors" name="major">
+            <datalist id="majors">
+                <option value="Computer Science">
+                <option value="Computer Engineering">
+                <option value="Mechanical Engineering">
+                <option value="Electrical Engineering">
+                <option value="All">
+            </datalist>
+            <input name="submit" type="submit" >
+        </form>
+    <body>
+<html>
+
+<?php
+if (isset($_POST['submit'])) 
+{
+    // replace ' ' with '\ ' in the strings so they are treated as single command line args
+    $major = escapeshellarg($_POST[major]);
+
+    $command = 'java -cp .:mysql-connector-java-5.1.40-bin.jar SearchJobs ' . $major;
+
+    // remove dangerous characters from command to protect web server
+    $escaped_command = escapeshellcmd($command);
+    echo "<p>command: $command <p>"; 
+    // run jdbc_insert_item.exe
+    system($escaped_command);           
+}
+?>
